a0c99c64e0a0b0c5c26d5d19b9ec38e65ec97e77
nadiah-m <nadiah.malik@Hotmail.com>
2022-01-24 14:58:00 +0800

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./Home/Home";
@@ -8,23 +8,6 @@ import About from "./About/About";
 import GetShow from "./GetShow/GetShow";
 
 function App() {
-  const [movies, setMovies] = useState({});
-
-  const getMovies = () => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/567189/recommendations?api_key=854888e6719c0f864d6db29cdfe47090&language=en-US&page=1"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const n = data.results[0];
-        setMovies(n);
-      });
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
     <div className="App">
       <nav>
