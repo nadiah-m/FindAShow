5f12ad4e15e7149a7539cacef0ae967281042f24
nadiah-m <nadiah.malik@Hotmail.com>
2022-01-26 18:08:35 +0800

    styling for favourites

--- src/Favourites/FavouritesList.jsx
+++ src/Favourites/FavouritesList.jsx
@@ -1,33 +1,49 @@
 import React from "react";
-import { useState } from "react";
+import RemoveIcon from "./RemoveIcon";
 import "./FavouritesList.css";
 
 function FavouritesList(props) {
   const baseImgURL = "https://image.tmdb.org/t/p/w300";
 
   const handleClick = () => {
-      console.log("Added to Fav");
-  }
+    console.log("Added to Fav");
+  };
 
   return (
-    <div className = "container-sm">
-      {props.favourites.map((show, index) => (
-        <div key={index}>
-          <div
-            
-            className="image-container flex-row d-flex flex-wrap justify-content-start row-cols-4"
-          >
-            <img onClick={handleClick} src={baseImgURL + show.poster_path} alt="show"></img>
+    <div className="container-fluid">
+      <div className="m-3">
+        <h5>Your Favourites</h5>
+      </div>
+      <div className="row row-cols-auto g-3 d-flex m-3">
+        {props.favourites.map((show, index) => (
+          <div key={index}>
+            <div className="image-container col overflow-auto">
+              <img
+                onClick={handleClick}
+                src={baseImgURL + show.poster_path}
+                alt="show"
+              ></img>
 
-            <div className="overlay d-flex align-items-center justify-content-center">
-              <div className="overflow-auto">
-                <p>{show.overview}</p>
-                <button onClick = {() => props.handleRemoveFavourites(show)}>Remove from Favourites</button>
+              <div className="overlay align-items-center overflow-scroll row row-cols-1">
+                <div className="col">
+                  <p>{show.overview}</p>
+                </div>
+                <div className="col">
+                  <p>User rating: {show.vote_average}</p>
+                  </div>
+                  <div className="col">
+                  <button type = "button" class = "btn btn-outline-light btn-sm" onClick={() => props.handleRemoveFavourites(show)}>
+                    <RemoveIcon />
+                    <div className="mr-2">
+                    Remove from Favourites
+                    </div>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
--- src/GetShow/components/ShowList.jsx
+++ src/GetShow/components/ShowList.jsx
@@ -8,7 +8,7 @@ const ShowList = (props) => {
     if (props.showTypeChecked === "tv") {
       return "TV series";
     } else if (props.showTypeChecked === "movie") {
-    return "Movies";
+      return "Movies";
     } else return;
   };
 
@@ -18,31 +18,32 @@ const ShowList = (props) => {
         <h5>{typeHeading()}</h5>
       </div>
       <div className="row row-cols-auto g-3 d-flex m-3">
-      {props.shows.map((show, index) => (
-        <div key={show.title}>
-          <div className="image-container col overflow-auto">
-            <img src={baseImgURL + show.poster_path} alt="show"></img>
+        {props.shows.map((show, index) => (
+          <div key={show.title}>
+            <div className="image-container col overflow-auto">
+              <img src={baseImgURL + show.poster_path} alt="show"></img>
 
-            <div className="overlay align-items-center overflow-scroll row row-cols-1">
-              <div className="col">
-                <p>{show.overview}</p>
-              </div>
-              <div class="col">
-                    <p>User rating: {show.vote_average}</p>
-                  </div>
-                  <div class="col">
-              <button type = "button" class = "btn btn-outline-light btn-sm" 
-              onClick={() => props.handleAddFavourites(show)}>
-                <HeartIcon />
-                <div className ="mr-2">
-                Add to Favourites
+              <div className="overlay align-items-center overflow-scroll row row-cols-1">
+                <div className="col">
+                  <p>{show.overview}</p>
                 </div>
-              </button>
+                <div class="col">
+                  <p>User rating: {show.vote_average}</p>
+                </div>
+                <div class="col">
+                  <button
+                    type="button"
+                    class="btn btn-outline-light btn-sm"
+                    onClick={() => props.handleAddFavourites(show)}
+                  >
+                    <HeartIcon /> 
+                    <div className="mr-2">Add to Favourites</div>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
