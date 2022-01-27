3638eef7dffbcaf8d407d98efb5099fa2807d2db
nadiah-m <nadiah.malik@Hotmail.com>
2022-01-27 17:07:04 +0800

    deployment

--- src/Search/SearchList.jsx
+++ src/Search/SearchList.jsx
@@ -61,43 +61,6 @@ function SearchList(props) {
       </div>
     </div>
 
-    //     <div>
-    //     <div className="container-fluid">
-    //       <div className="m-3">
-    //         <h5>{props.searchValue}</h5>
-    //       </div>
-    //       <div className="row row-cols-auto g-3 d-flex flex-nowrap m-3 overflow-auto">
-    //         {props.searchList.map((searchItem) => (
-    //           <div key={searchItem.original_title}>
-    //             <div className="image-container col overflow-auto">
-    //               <img
-    //                 src={baseImgURL + searchItem.poster_path}
-    //                 alt="show"
-    //               ></img>
-    //               <div className="overlay align-items-center overflow-scroll row row-cols-1">
-    //                 <div className="col">
-    //                   <p>{searchItem.overview}</p>
-    //                 </div>
-    //                 <div className="col">
-    //                   <p>User rating: {searchItem.vote_average}</p>
-    //                 </div>
-    //                 <div className="col">
-    //                   <button
-    //                     type="button"
-    //                     className="btn btn-outline-light btn-sm"
-    //                     onClick={() => props.handleAddFavourites(searchItem)}
-    //                   >
-    //                     <HeartIcon />
-    //                     <div className="mr-2">Add to Favourites</div>
-    //                   </button>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
   );
 }
 export default SearchList;
